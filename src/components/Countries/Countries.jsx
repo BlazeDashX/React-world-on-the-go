33077cd1d8ed2fef6de0b12e73894cf142d77c37
BlazeDashX <refat00021@gmail.com>
2026-05-10 04:16:41 +0600

    Implement country fetching and display with loading state; add styling for countries and individual country components

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,8 +1,22 @@
+import { use } from "react";
+import './Countries.css';
+import Country from "../Country/Country";
 
-const Countries = () => {
+const Countries = ({ countriesPromise }) => {
+    const countriesData = use(countriesPromise);
+    const countries = countriesData.countries;
     return (
-        <div>
-            <h1>In the Countries</h1>
+        <div >
+            <h1>In the Countries: {countries.length}</h1>
+
+            <div className="Countries">
+                {countries.map(country => (
+                <Country
+                    key={country.cca3.cca3}
+                    country={country}
+                ></Country>
+            ))}
+            </div>
         </div>
     );
 };
---
+++ src/components/Country/Country.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import './Country.css';
+
+const Country = ({ country }) => {
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = () => { 
+        setVisited(visited ? false : true);
+        // console.log('Visited: ', country.name.common);
+    };   
+
+    return (
+        <div className={`Country ${visited ? 'Country-visited' : 'Country-not-visited'}`}>
+            <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
+            <h3>Name: {country.name.common}</h3>
+            <p>Population: {country.population.population}</p>
+            <p>Area: {country.area.area} {
+                country.area.area > 10000 ? "Big Country": "Small Country"   
+            }</p>
+            <button onClick={handleVisited}>
+                {visited ? "Visited": "Not Visited"}
+            </button>
+        </div>
+    );
+};
+
+export default Country;
